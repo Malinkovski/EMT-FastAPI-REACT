43aa7f8f8f88a23443fca87bf360a80c36c2c738
Ilija Malinkovski <ilija.malinkovski@gmail.com>
2026-03-08 17:08:58 +0100

    added router + main config

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from fastapi import FastAPI
+from web import accommodation_router
 
 app = FastAPI()
 
+app.include_router(accommodation_router.router)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
