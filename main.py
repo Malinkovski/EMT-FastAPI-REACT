85095bac568b58134049ea120478d214b2fb5eb8
unknown <ilija.malinkovski@students.finki.ukim.mk>
2026-03-13 17:04:51 +0100

    added db (ADD ITT THROUGH DB SQLITE)

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
 from web import accommodation_router
 
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from database.database import engine
+from model.models import Base
+
+Base.metadata.create_all(bind=engine)
+
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     seed()
+#     yield
+
+# app = FastAPI(lifespan=lifespan)
 app = FastAPI()
 
 app.include_router(accommodation_router.router)
